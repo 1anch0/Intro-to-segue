92b7f5e7f4819b60e3bd2e168df14bb565f3bd6b
1anch0 <110996577+1anch0@users.noreply.github.com>
2022-08-11 10:05:47 -0400

    Add files via upload

---
+++ Intro-to-Segues/ViewController2.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController2.swift
+//  Intro-to-Segues
+//
+//  Created by Scholar on 8/11/22.
+//
+
+import UIKit
+
+class ViewController2: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
